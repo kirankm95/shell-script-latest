21bfd8374a4ce60508624aadda094b22435174d5
Kiran <kirankm95@gmail.com>
2025-08-08 13:26:31 +0530

    new shell script

---
+++ 02-addition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ARG1=$1
+ARG2=$2
+SUM=$(($ARG1+$ARG2))
+
+echo "the sum of given two numbers is:: ${SUM}"
---
+++ 03-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+array1=("Apple" "Grapes" "Banana "watermelon")
+
+echo "array1 itme is: ${array1[0]}"
+echo "array1 itme is: ${array1[1]}"
+echo "array1 itme is: ${array1[2]}"
+echo "array1 itme is: ${array1[3]}"
+
+#echo "all array itmes are: ${array1[@]}"
