4c99b3f48b7a80a0197f042cdbfeeb39a6eaf479
Kiran <kirankm95@gmail.com>
2025-08-09 12:49:55 +0530

    new shell script

--- 06-mysql.sh
+++ 06-mysql.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 ID=$(id -u)
 
-echo "$ID"
+if [ $ID -eq 0 ]
+then
+    echo "Hello root, we are proceeding"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+        echo "mysql installation is success"
+    else
+        echo "mysql installation is failed"
+
+else
+    echo "you are not root user, hence exiting from this script"
+    exit
+fi
