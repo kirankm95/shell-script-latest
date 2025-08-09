24d8e49cf555ff37e4debac6fb59a7cba4b1eda8
Kiran <kirankm95@gmail.com>
2025-08-09 12:12:45 +0530

    new shell script

--- 02-addition.sh
+++ 02-addition.sh
@@ -4,3 +4,10 @@ ARG2=$2
 SUM=$(($ARG1+$ARG2))
 
 echo "the sum of given two numbers is:: ${SUM}"
+
+echo "first argument passed: ${ARG1}"
+echo "second argument passed: ${ARG2}"
+echo "last argument passwd: $N"
+echo "all arguments passed: $@"
+echo "total number of arguments passed: $#"
+echo "total number of arguments passed: $*"
