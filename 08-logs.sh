07b569d91a3415a0b9d8dfa62d2d7e1ed306f1bf
Kiran <kirankm95@gmail.com>
2025-08-09 13:30:51 +0530

    new shell script

--- 08-logs.sh
+++ 08-logs.sh
@@ -3,7 +3,7 @@ ID=$(id -u)
 TIME=$(date +%F-%H-%M-%S)
 LOGFILE=$/tmp/$0-$TIME.log
 
-echo "this script started executing at ${TIME}"
+echo "this script started executing at ${TIME}" &>> $LOGFILE
 
 validate(){
     if [ $1 -eq 0 ]
@@ -19,8 +19,8 @@ then
     exit 1
 else
     echo "you are root user, hence proceeding"
-    dnf install mysql -y
+    dnf install mysql -y &>> $LOGFILE
     validate $? mysql
-    dnf install git -y
+    dnf install git -y &>> $LOGFILE
     validate $? git
 fi
