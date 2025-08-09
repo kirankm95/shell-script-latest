7d4544425b3e243ddb376dd05b72563bad66217d
Kiran <kirankm95@gmail.com>
2025-08-09 13:17:40 +0530

    new shell script

--- 07-mysql-git.sh
+++ 07-mysql-git.sh
@@ -2,23 +2,21 @@
 
 ID=$(id -u)
 
+validate(){
+    if [ $? -eq 0 ]
+    then
+        echo "package installed success"
+    else
+        echo "package installed failed"
+    fi
+}
 if [ $ID -ne 0 ]
 then
     echo "you are not root user, please re-try with root user"
 else
     echo "you are root user, hence proceeding"
     dnf install mysql
-    if [ $? -eq 0 ]
-    then
-        echo "package mysql installed success"
-    else
-        echo "package mysql installed failed"
-    fi
+    validate
     dnf install git
-    if [ $? -eq 0 ]
-    then
-        echo "package git installed success"
-    else
-        echo "package git installed failed"
-    fi
+    validate
 fi
