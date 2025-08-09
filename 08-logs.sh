79b677e11dda521593b8dd71570b203df733d7e5
Kiran <kirankm95@gmail.com>
2025-08-09 13:47:22 +0530

    new shell script

--- 08-logs.sh
+++ 08-logs.sh
@@ -2,23 +2,27 @@
 ID=$(id -u)
 TIME=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIME.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 echo "this script started executing at ${TIME}" &>> $LOGFILE
 
 validate(){
     if [ $1 -eq 0 ]
     then
-        echo "$2 installed success"
+        echo "$2 installed $G success $N"
     else
-        echo "$2 installed failed"
+        echo "$2 installed $R failed $N"
     fi
 }
 if [ $ID -ne 0 ]
 then
-    echo "you are not root user, please re-try with root user"
+    echo "$Y you are not root user, please re-try with root user $N"
     exit 1
 else
-    echo "you are root user, hence proceeding"
+    echo "$G you are root user, hence proceeding $N"
     dnf install mysql -y &>> $LOGFILE
     validate $? mysql
     dnf install git -y &>> $LOGFILE
