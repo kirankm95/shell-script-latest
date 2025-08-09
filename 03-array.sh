3442e498f0bf86d9476e0f23e37bcbe018744894
Kiran <kirankm95@gmail.com>
2025-08-09 12:37:49 +0530

    new shell script

--- 03-array.sh
+++ 03-array.sh
@@ -4,4 +4,3 @@ array1=("Apple" "Grapes" "Banana "watermelon")
 echo "array1 itme is: ${array1[0]}"
 echo "array1 itme is: ${array1[1]}"
 echo "array1 itme is: ${array1[2]}"
-echo "array1 itme is: ${array1[3]}"
