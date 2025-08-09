f883559c3b5e8c8adf6509ba4cc5a8c6979f58c3
Kiran <kirankm95@gmail.com>
2025-08-09 13:04:49 +0530

    new shell script

--- 06-mysql.sh
+++ 06-mysql.sh
@@ -4,13 +4,22 @@ ID=$(id -u)
 if [ $ID -eq 0 ]
 then
     echo "Hello root, we are proceeding"
-    dnf install mysql -y
-    if [ $? -eq 0 ]
+    echo "checking if package already installed or not"
+    package=$(yum list installed mysql)
+    if [ $? -eq 0 ] 
     then
-        echo "mysql installation is success"
+        echo "package already installed, so exiting"
+        exit 1
     else
-        echo "mysql installation is failed"
+        echo "package not installed, so proceeding further"
     fi
+    dnf install mysql -y
+        if [ $? -eq 0 ]
+        then
+            echo "mysql installation is success"
+        else
+            echo "mysql installation is failed"
+        fi
 else
     echo "you are not root user, hence exiting from this script"
     exit 1
