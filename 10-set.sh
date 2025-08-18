ab5e05696743a47b25af7b92dd62a33309641553
Kiran <kirankm95@gmail.com>
2025-08-18 12:50:17 +0530

    new shell script

---
+++ 10-set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "start of the script"
+
+pwd
+
+echo "after pwd command"
+
+echo "before wrong command"
+
+pwdq
+
+echo "after wrong command"
---
+++ 11-delete-logs-files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+ID=$(id -u)
+TIME=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIME.log"
+SOURCE-DIR="/tmp/shell-script-logs"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"s
+
+echo -e "this script started executing at ${TIME}" &>> $LOGFILE
+
+validate(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 installed $G success $N"
+    else
+        echo -e "$2 installed $R failed $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+    then
+        echo -e "$Y you are not root user, please re-try with root user $N"
+        exit 1
+    else
+        echo -e "$G you are root user, hence proceeding $N"
+fi
+
+if [ ! -d $SOURCE-DIR ]
+then
+    echo -e "$R ${SOURCE-DIR} does not exist $N"
+    exit 1
+else
+    echo -e "$G ${SOURCE-DIR} exist, so proceeding $N"
+fi
+
+
+DELETE=$(find $SOURCE-DIR -name *.log -mtime +30 -type f)
+
+while IFS= read -r line
+do
+    echo "deleting line: $line"
+    rm -rf $line
+done <<< DELETE &>> $LOGFILE
+
+validate $? "deleting log files"
