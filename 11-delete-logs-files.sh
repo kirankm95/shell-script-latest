cbed981d15c0838731698b93ca22f0deeb03f074
Kiran <kirankm95@gmail.com>
2025-08-18 12:58:26 +0530

    new shell script

--- 11-delete-logs-files.sh
+++ 11-delete-logs-files.sh
@@ -2,11 +2,11 @@
 ID=$(id -u)
 TIME=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIME.log"
-SOURCE-DIR=`echo "/tmp/shell-script-logs"`
+SOURCE_DIR="/tmp/shell-script-logs"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e[33m"s
+Y="\e[33m"
 
 echo -e "this script started executing at ${TIME}" &>> $LOGFILE
 
@@ -27,16 +27,16 @@ if [ $ID -ne 0 ]
         echo -e "$G you are root user, hence proceeding $N"
 fi
 
-if [ ! -d $SOURCE-DIR ]
+if [ ! -d $SOURCE_DIR ]
 then
-    echo -e "$R ${SOURCE-DIR} does not exist $N"
+    echo -e "$R $SOURCE_DIR does not exist $N"
     exit 1
 else
-    echo -e "$G ${SOURCE-DIR} exist, so proceeding $N"
+    echo -e "$G $SOURCE_DIR exist, so proceeding $N"
 fi
 
 
-DELETE=$(find $SOURCE-DIR -name *.log -mtime +30 -type f)
+DELETE=$(find $SOURCE_DIR -name *.log -mtime +30 -type f)
 
 while IFS= read -r line
 do
